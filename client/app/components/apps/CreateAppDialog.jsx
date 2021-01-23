6d6900adb0924f58ec61674fc7fae8e8b1b1ae6f
zhe.li <zhe.li@ihandysoft.com>
2021-01-23 20:00:07 +0800

    Redash - 设置页面添加 app 列表管理（实现创建 app 和 删除 app 对话框）

--- client/app/components/apps/CreateAppDialog.jsx
+++ client/app/components/apps/CreateAppDialog.jsx
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import Button from "antd/lib/button";
 import Modal from "antd/lib/modal";
-import Input from "antd/lib/input";
+import Alert from "antd/lib/alert";
+import DynamicForm from "@/components/dynamic-form/DynamicForm";
 import { wrap as wrapDialog, DialogPropType } from "@/components/DialogWrapper";
+import recordEvent from "@/services/recordEvent";
 
-class CreateAppDialog extends React.Component {
-  static propTypes = {
-    dialog: DialogPropType.isRequired,
-  };
+const formFields = [
+  { required: true, name: "name", title: "Name", type: "text", autoFocus: true },
+  { required: false, name: "icon_url", title: "Icon Url", type: "text" },
+  { required: false, name: "description", title: "Description", type: "text" },
+];
 
-  state = {
-    name: "",
-  };
+function CreateAppDialog({ dialog }) {
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    recordEvent("view", "page", "apps/new");
+  }, []);
 
-  save = () => {
-    this.props.dialog.close({
-      name: this.state.name,
-    });
-  };
+  const handleSubmit = useCallback(values => dialog.close(values).catch(setError), [dialog]);
 
-  render() {
-    const { dialog } = this.props;
-    return (
-      <Modal {...dialog.props} title="Create a New App" okText="Create" onOk={() => this.save()}>
-        <Input
-          className="form-control"
-          defaultValue={this.state.name}
-          onChange={event => this.setState({ name: event.target.value })}
-          onPressEnter={() => this.save()}
-          placeholder="App Name"
-          autoFocus
-        />
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      {...dialog.props}
+      title="Create a New App"
+      footer={[
+        <Button key="cancel" {...dialog.props.cancelButtonProps} onClick={dialog.dismiss}>
+          Cancel
+        </Button>,
+        <Button
+          key="submit"
+          {...dialog.props.okButtonProps}
+          htmlType="submit"
+          type="primary"
+          form="appForm"
+          data-test="SaveAppButton">
+          Create
+        </Button>,
+      ]}
+      wrapProps={{
+        "data-test": "CreateAppDialog",
+      }}>
+      <DynamicForm id="appForm" fields={formFields} onSubmit={handleSubmit} hideSubmitButton />
+      {error && <Alert message={error.message} type="error" showIcon data-test="CreateAppErrorAlert" />}
+    </Modal>
+  );
 }
+
+CreateAppDialog.propTypes = {
+  dialog: DialogPropType.isRequired,
+};
 
 export default wrapDialog(CreateAppDialog);
